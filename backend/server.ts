6f3f679a58b157434ef00f2a2f8c30b562da4e1e
Getye <getye2008@gmail.com>
2024-08-23 15:21:47 +0300

    v3

--- backend/server.ts
+++ backend/server.ts
@@ -1,18 +1,20 @@
 import express, { application } from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv'
 import songsRouter from './routes/songRoutes';
 
 const app= express();
 //app.use(cors());
+dotenv.config();
 app.use(cors({
   origin: 'https://inquisitive-griffin-a97ff5.netlify.app', 
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: 'Content-Type, Authorization',
 }));
 app.use(express.json());
-
-mongoose.connect('mongodb://mongo:27017/musicdb').then(() => {
+const mongoUrl = process.env.MONGO_URL;
+mongoose.connect(mongoUrl as string ).then(() => {
   console.log('Connected to MongoDB');
 }).catch((error) => {
   console.error('Error connecting to MongoDB:', error);
